07d63cd556b88079a7aa8c9d69d6ddbed298868e
karanfx <karanmirajkar51@gmail.com>
2023-07-29 22:01:36 +0530

    media root changes

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-jwlyam&)5nk0bo-gare@mpcx1ino2)^544)duqk-$gr6ne&+il
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = ['198.211.99.20', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app','198.211.99.20', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -117,13 +117,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static'),
-# ]
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
