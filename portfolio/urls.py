30588b7173fb194d7c819f008ae119be230521bf
karanfx <karanmirajkar51@gmail.com>
2023-07-29 17:14:34 +0530

    Updated for vercel

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('portfolio/',views.portfolio, name='portfolio'),
-    path('',views.home,name='home'),
+    # path('portfolio/',views.portfolio, name='portfolio'),
+    # path('',views.home,name='home'),
+    path('',views.portfolio, name='portfolio'),
 
 ]
--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-jwlyam&)5nk0bo-gare@mpcx1ino2)^544)duqk-$gr6ne&+il
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
